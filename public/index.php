6cab67f0a4fb94de6196e90b163ed27b5393aeb6
Morris Chavez <morris@fastfwd.com>
2020-12-23 10:48:35 +0800

    - convert RAM computation to object

--- public/index.php
+++ public/index.php
@@ -1,4 +1,35 @@
 <?php
+
+class RAM
+{
+    const COST_PER_GIG_PER_HOUR = 0.00553;
+    const RAM_INCREMENT = 500;
+    const STUDIES_TO_RAM_RATIO = 0.5;
+
+    public function __construct(private int $studies) {}
+
+    /**
+     * @param  int  $days
+     * @return float|int
+     */
+    public function computeCostByDays(int $days)
+    {
+        return $this->totalRAMHourlyCost()
+            * 24 // daily cost
+            * $days;
+    }
+
+    public function totalRAM()
+    {
+        return ceil(($this->studies * self::STUDIES_TO_RAM_RATIO) / self::RAM_INCREMENT) * self::RAM_INCREMENT ;
+    }
+
+    public function totalRAMHourlyCost()
+    {
+        return ($this->totalRAM() / 1000) * self::COST_PER_GIG_PER_HOUR;
+    }
+}
+
 if (!empty($_POST)) {
     $now = new DateTime();
 
@@ -15,15 +46,19 @@ if (!empty($_POST)) {
     echo '<table>';
     echo '<tbody>';
     for ($month = 1; $month <= $months; $month++) {
+        $currentMonth = (new DateTime())->modify('+' . $month . ' month');
+        $daysInMonth = $currentMonth->format('t');
 
-        $study = $study * (1 + $studyGrowth * $month );
-        $ramCost = 0.0053 * 24 * 30 * ($study/2); // say one month is 30 days
-        $storageCost = $study/1000 * .1; // $0.10 per 1GB
+        $ramCost = (new RAM($study))->computeCostByDays($daysInMonth);
+//        $storageCost = $study * Storage::STUDY_TO_STORAGE_RATIO * Storage::COST_PER_GB;
+        $storageCost = ($study * .5) / 1000 * 0.1;
         echo '<tr>';
-        echo '<td>' . $now->modify('+' . $month . ' month')->format('M Y') . '</td>';
+        echo '<td>' . $currentMonth->format('M Y') . "|$daysInMonth" . '</td>';
         echo '<td>' . $study . '</td>';
-        echo '<td>' . $ramCost + $storageCost . '</td>';
+        echo '<td>' . $ramCost . '|' . $storageCost . ' </td>';
         echo '</tr>';
+
+        $study = $studyCount * (1 + $studyGrowth * $month );
     }
     echo '</tbody>';
     echo '</table>';
