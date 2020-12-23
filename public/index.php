f398470754cba63ce1f32456fd653c5718f997fc
Morris Chavez <morris@fastfwd.com>
2020-12-23 15:19:38 +0800

    removes error display

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors', true);
-
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'src' . DIRECTORY_SEPARATOR);
 define('ASSETS', DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR);
@@ -10,7 +8,7 @@ include_once ROOT . 'vendor/autoload.php';
 try {
     $application = new \LSM\Application();
     $application->run();
-} catch(Exception $e) {
+} catch (Exception $e) {
     header("HTTP/1.0 {$e->getCode()}");
     echo $e->getMessage();
 }
