fb56a151147e485d3128853a75199bd37813e68a
Morris Chavez <morris@fastfwd.com>
2020-12-23 14:45:12 +0800

    form styling

--- src/Views/index.php
+++ src/Views/index.php
@@ -1,6 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/app.css"/>
+    <title>LSM Cost Forecaster</title>
 </head>
 <body>
 <div class="container">
